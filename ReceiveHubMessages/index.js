ba8806888958fe5830e3840e182892b03b41112a
Justine Cocchi <jucocchi@microsoft.com>
2017-06-26 15:18:41 -0700

    write message data to cosmosdb

--- ReceiveHubMessages/index.js
+++ ReceiveHubMessages/index.js
@@ -1,8 +1,25 @@
 require('dotenv').config()
+const assert = require('assert')
 const EventHubClient = require('azure-event-hubs').Client
+const MongoClient = require('mongodb').MongoClient
 
-const connString = process.env.EVENT_HUB_CONN_STRING
-const client = EventHubClient.fromConnectionString(connString)
+// Set up event hub connection
+const eventHubConnString = process.env.EVENT_HUB_CONN_STRING
+const client = EventHubClient.fromConnectionString(eventHubConnString)
+
+// Set up CosmosDB MongoDB connection
+const mongoConnString = process.env.COSMOS_CONN_STRING
+
+function insertDocument (db, msg, cb) {
+  db.collection(process.env.COLLECTION_ID).insertOne({
+    deviceId: msg.deviceId,
+    temp: msg.temperature,
+    hum: msg.humidity
+  }, function (err, res) {
+    assert.equal(err, null)
+    cb()
+  })
+}
 
 function printError (err) {
   console.log(err.message)
@@ -11,7 +28,16 @@ function printError (err) {
 function printMessage (message) {
   console.log('Message received: ')
   console.log(JSON.stringify(message.body))
-  console.log('')
+
+  // Write message to CosmosDB
+  MongoClient.connect(mongoConnString, function (err, db) {
+    assert.equal(null, err)
+    insertDocument(db, message.body, function () {
+      console.log('Successfully inserted the message to CosmosDB.')
+      console.log('')
+      db.close()
+    })
+  })
 }
 
 client.open()
