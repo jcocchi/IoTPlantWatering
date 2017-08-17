363025478a01805f101a6ac894fdafcd67101bd1
Jennelle Crothers <jkcrothers@hotmail.com>
2017-08-17 15:10:10 -0700

    Automated VM creation with DSC

---
+++ Scripts/VM/ClientWebServerDSConfig.ps1
@@ -0,0 +1,37 @@
+Configuration ClientWebServer            
+{            
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, cChoco, xSystemSecurity          
+    Node localhost            
+    {       
+      cChocoInstaller installChoco
+      {
+        InstallDir = "C:\Program Files (x86)\choco"
+      }
+      
+      cChocoPackageInstaller installNodejs-lts
+      {
+         Ensure = 'Present'
+         Name = "nodejs-lts"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+
+      cChocoPackageInstaller installvisualstudiocode
+      {
+         Ensure = 'Present'
+         Name = "visualstudiocode"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+     
+      xIEEsc EnableIEEscAdmin
+        {
+            IsEnabled = $False
+            UserRole  = "Administrators"
+        }
+        xIEEsc EnableIEEscUser
+        {
+            IsEnabled = $False
+            UserRole  = "Users"
+        }
+
+    }
+}
